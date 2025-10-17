1ceed9adf31daa279c12c4e9afef44bb639d5083
Jul-svoboda <july.svoboda@gmail.com>
2025-10-17 17:26:34 +0300

    feat: implement reverseWords

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -380,8 +380,13 @@ function findLongestWord(sentence) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const strArry = str.split(' ');
+  let newStr = '';
+  for (let i = 0; i < strArry.length; i += 1) {
+    newStr += `${strArry[i].split('').reverse().join('')} `;
+  }
+  return newStr.trimEnd();
 }
 
 /**
